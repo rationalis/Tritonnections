94a77a48d4635f40aafdf1a462c410d1f9bb8554
Amin-Fz <afozi732@gmail.com>
2016-11-06 12:21:07 -0800

    Slight difference

---
+++ app/src/main/java/com/ucsdcse110/tritonnections/LoadQuartersTask.java
@@ -0,0 +1,34 @@
+package com.ucsdcse110.tritonnections;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.HashMap;
+
+/**
+ * Created by Amin on 11/6/2016.
+ */
+
+public class LoadQuartersTask extends HTTPRequestTask<HashMap<String,String>> {
+    @Override
+    protected HashMap<String, String> doInBackground(String... params) {
+        String url = "https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudent.htm";
+        String html = request(url,null,"GET");
+
+        Document doc = Jsoup.parse(html);
+        Element quarterList = doc.select("select#selectedTerm").get(0);
+        Elements quarters = quarterList.select("option");
+
+        HashMap<String,String> terms = new HashMap<>();
+
+        for (Element quarter : quarters) {
+            terms.put(quarter.text(), quarter.val());
+        }
+
+
+
+        return null;
+    }
+}

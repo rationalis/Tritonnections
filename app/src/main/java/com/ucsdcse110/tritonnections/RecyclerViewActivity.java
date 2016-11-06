45ae7b968eaff99779420eabdb73f36a39e56a8a
KiraMelody <jessie_tjq@163.com>
2016-11-06 11:14:42 -0800

    temp

--- app/src/main/java/com/ucsdcse110/tritonnections/RecyclerViewActivity.java
+++ app/src/main/java/com/ucsdcse110/tritonnections/RecyclerViewActivity.java
@@ -2,13 +2,8 @@ package com.ucsdcse110.tritonnections;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +17,7 @@ public class RecyclerViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.recyclerview_activity);
+        setContentView(R.layout.activity_recyclerview);
 
         rv=(RecyclerView)findViewById(R.id.rv);
 

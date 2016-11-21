b76403fc11ea505e663bb22dbf60e6fa655e4cac
Amin-Fz <afozi732@gmail.com>
2016-11-20 20:21:39 -0800

    Made a simple forum post class

---
+++ app/src/main/java/com/ucsdcse110/tritonnections/ForumPost.java
@@ -0,0 +1,14 @@
+package com.ucsdcse110.tritonnections;
+
+/**
+ * Created by Amin on 11/20/2016.
+ */
+
+public class ForumPost {
+    private ForumFragment parent;
+    private String user;
+    private String date;
+
+    private String content;
+
+}

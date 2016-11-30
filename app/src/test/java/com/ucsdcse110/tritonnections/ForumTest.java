a8a28fe83eccea14d6571e0baf8836083159b1f0
Amin-Fz <afozi732@gmail.com>
2016-11-30 12:31:15 -0800

    Created test classes.

---
+++ app/src/test/java/com/ucsdcse110/tritonnections/ForumTest.java
@@ -0,0 +1,15 @@
+package com.ucsdcse110.tritonnections;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Amin on 11/30/2016.
+ */
+public class ForumTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
